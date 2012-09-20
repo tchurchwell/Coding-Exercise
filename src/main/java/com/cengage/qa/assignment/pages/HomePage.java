bf827119905351c0afa9a4faf1d7f3cc1baf0c8f
Brent Velandra <brent.velandra@cengage.com>
2012-09-20 16:55:45 -0400

    modified assignment instructions

--- src/main/java/com/cengage/qa/assignment/pages/HomePage.java
+++ src/main/java/com/cengage/qa/assignment/pages/HomePage.java
@@ -6,28 +6,28 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class HomePage extends BasePage {
-    
-    public HomePage() {
-        super(PRODUCT_URL);
-    }
-    
-    public HomePage(WebDriver driver) {
-    	super(driver);
-    }
-    
-    public HomePage(String url) {
-    	 super(url);
-    }
-    
-    public AwardsRewardedPage gotoAwardsReceivedPage() {
-    	driver.findElement(By.linkText("News")).click();
-    	driver.findElement(By.partialLinkText("Awards Received")).click();
-    	return new AwardsRewardedPage(driver);
-    }
-    
-    public AwardsGivenPage gotoAwardsGivenPage() {
-      driver.findElement(By.linkText("» Awards Given")).click();
-      return new AwardsGivenPage(driver);
-    }
-    
+
+	public HomePage() {
+		super(PRODUCT_URL);
+	}
+
+	public HomePage(WebDriver driver) {
+		super(driver);
+	}
+
+	public HomePage(String url) {
+		super(url);
+	}
+
+	public AwardsRewardedPage gotoAwardsReceivedPage() {
+		driver.findElement(By.linkText("News")).click();
+		driver.findElement(By.partialLinkText("Awards Received")).click();
+		return new AwardsRewardedPage(driver);
+	}
+
+	public AwardsGivenPage gotoAwardsGivenPage() {
+		driver.findElement(By.linkText("Awards Given")).click();
+		return new AwardsGivenPage(driver);
+	}
+
 }
